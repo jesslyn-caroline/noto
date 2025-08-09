383491935bbd079cd286e77488f766a212ab1a95
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-08-10 00:00:16 +0700

    Update content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
-import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import 'remixicon/fonts/remixicon.css';
 
-const montserrat = Montserrat({
+import NavBar from "./components/navbar";
+import NotFound from "./not-found";
+import ThemeProviders from "./themeprovider";
+
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
+import SideBar from "./components/sidebar";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
@@ -11,10 +19,22 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const theme:string = "light";
+
   return (
-    <html lang="en">
-      <body className={`${montserrat.className} antialiased`}>
-        {children}
+    <html lang="en" className={`${theme} scroll-bar-hidden`} suppressHydrationWarning>
+      <body className={`${poppins.className} antialiased flex justify-center overflow-x-hidden overflow-y-scroll`}>
+        <ThemeProviders>
+          <ErrorBoundary fallback={<NotFound />}>
+            <div className={`max-w-[1920px] min-h-screen w-screen`}> 
+              {/* <SideBar>
+                {children}
+              </SideBar> */}
+              {children}
+            </div>
+          </ErrorBoundary>  
+        </ThemeProviders>
       </body>
     </html>
   );

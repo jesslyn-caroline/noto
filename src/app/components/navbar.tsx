0130f725dc62e06cfaacc30ce3aae72e64a7140e
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-08-10 00:01:36 +0700

    New component

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NavBar({isOpen, toggleSideBar} : {isOpen: boolean, toggleSideBar: () => void}) {
+  return (
+    <div className={`w-full h-fit px-5 py-1 bg-background fixed border-b border-gray-300 flex items-center gap-x-10`}>
+        <button type="button" title={`${isOpen? 'Close Menu' : 'Open Menu'}`} onClick={toggleSideBar} className={`${isOpen? 'hidden' : 'block'}`}>
+          <i className={`ri-menu-line text-xl`} />
+        </button>
+        <h1 className={`text-lg font-medium`}>Notō <i className={`ri-booklet-line font-normal`}/></h1>
+    </div>
+  )
+}
---
+++ src/app/components/sidebar.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import React, {useState} from 'react'
+import NavBar from './navbar';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import ModeButton from './button/mode-button';
+
+export default function SideBar({children} : {children: React.ReactNode}) {
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleSideBar = ():void => {
+      setIsOpen(!isOpen);
+  }
+
+  const pathName = usePathname();
+
+  const paths = [
+    { path: '/', name: 'Home' },
+    { path: '/dummy', name: 'Dummy' },
+  ]
+
+  return (
+    <div className={`w-full h-full flex flex-row transition-all `}>
+      <div className={`${isOpen? 'block lg:hidden' : 'hidden'} translate-0 fixed w-full h-full bg-black opacity-50 z-10`}></div>
+      <div className={`${isOpen? 'translate-x-0 visible' : '-translate-x-full invisible'} fixed px-5 py-1 bg-background min-w-52 w-52 h-full z-20 border-r-1 border-r-gray-200 flex flex-col items-start transition-all`}>
+        <button title={`${isOpen? 'Close Menu' : 'Open Menu'}`} onClick={toggleSideBar}>
+          <i className={`ri-menu-fold-line text-xl`} />
+        </button> 
+        {
+          ...paths.map((route) => {
+            console.log(route.path === pathName);
+            return (
+              <Link href={route.path} className={`flex gap-x-5 items-center py-1`}>
+                <span className={`text-sm w-fit ${route.path === pathName ? 'font-semibold' : 'font-normal'}`}>{route.name}</span>
+              </Link>
+            )
+          })
+        }
+        <ModeButton />
+      </div>
+      <div className={`${isOpen? 'block' : 'hidden'} min-w-52 w-52`}></div>
+      <div className={`w-full h-full`}>
+        <NavBar isOpen={isOpen} toggleSideBar={toggleSideBar} />
+        { children }
+      </div>
+    </div>
+  )
+}
